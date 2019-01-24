c293ff4c519d2ae82756cac2815be5b3b4bbc656
bernard-ng <ngandubernard@gmail.com>
2019-01-24 20:07:22 +0200

    [Update] setup basic config

--- index.php
+++ index.php
@@ -1,2 +1,12 @@
 <?php
 require_once(__DIR__ . "/vendor/autoload.php");
+
+use AdminTwig\Router\Route;
+use AdminTwig\Application;
+
+
+$router = new Router($_SERVER['REQUEST_URI'] ?? '/');
+require(__DIR__ . "src/routes.php");
+
+$app = new Application();
+$app->run();
